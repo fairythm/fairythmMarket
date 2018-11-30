94813ff71da1cb5dcbdd16cf7cef48bcea9e815b
NJUPT_B16041711 <lostedfactory@163.com>
2018-11-30 21:38:30 +0800

    version1

--- app/src/main/java/com/fairythm/fairythmmarket/Categories.java
+++ app/src/main/java/com/fairythm/fairythmmarket/Categories.java
@@ -1,5 +1,11 @@
 package com.fairythm.fairythmmarket;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -8,7 +14,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import java.util.List;
 
 public class Categories extends AppCompatActivity {
 
@@ -24,38 +33,63 @@ public class Categories extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //toolbar导航栏设置
-        categoryDrawerLayout = findViewById(R.id.categories_drawer);
+        categoryDrawerLayout = findViewById(R.id.categories_drawer_layout);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_navigator);
         }
 
+        //Drawer中点击事件设置
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.appList);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.appList:
+                        getPKList(Categories.this);
+                        categoryDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.application:
+
+                        categoryDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.game:
+
+
+                        categoryDrawerLayout.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
-    //toolbar添加menu
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.categories_toolbar_menu,menu);
-        return true;
+    //获取已安装应用列表
+    List<PackageInfo> getPKList(Context context){
+        PackageManager pm = context.getPackageManager();
+        List<PackageInfo> ip = pm.getInstalledPackages(0);//获取全部安装的应用
+        int flag = PackageManager.GET_ACTIVITIES + PackageManager.GET_PERMISSIONS;//0
+
+        Log.e("Categories","starting to scan apps");
+        for(int i = 0;i < ip.size();i++){
+            Log.e("Package:",ip.get(i).packageName);
+        }
+
+        return ip;
     }
 
-    //按下menu选项
-    public boolean onOptionsItemSelected(MenuItem item){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()){
             case android.R.id.home:
-                categoryDrawerLayout.openDrawer(GravityCompat.START);//打开drawer
-                break;
-            case R.id.application:
-                //显示应用
-                break;
-            case R.id.game:
-                //显示游戏
-                break;
-            case R.id.settings:
-                //显示设置
+                categoryDrawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
         }
+
         return true;
     }
 }
---
+++ app/src/main/java/com/fairythm/fairythmmarket/LocalApps.java
@@ -0,0 +1,13 @@
+package com.fairythm.fairythmmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalApps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_apps);
+    }
+}
